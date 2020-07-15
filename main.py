726072672740b19e6b17892afa8b505c9057daeb
Alina Robu <alina.robu@realtech.com>
2020-07-15 21:37:55 +0300

    Tema 1

---
+++ main.py
@@ -0,0 +1,12 @@
+a = [1, 2, 3, 4, 5]
+a.reverse()
+print(a)
+
+a = [1, 1, 2, 2, 2, 2, 3, 3, 3]
+b = 2
+count = a.count(b)
+print(count)
+
+a = 'ana are mere si nu are pere'
+words = len(a.split())
+print(words)
